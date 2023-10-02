527dc871bd4fd2368e23d51e42c97ff2559a2d82
Greg <gmerrill03@gmail.com>
2023-10-02 00:11:49 -0700

    begin planning out the data

--- components/Home.js
+++ components/Home.js
@@ -1,7 +1,55 @@
-import { View, Text, SafeAreaView } from 'react-native'
+import { View, Text, SafeAreaView, FlatList } from 'react-native'
 
 import styles from '../styles/home'
 import { useEffect, useState } from 'react'
+
+const DATA = [
+  {
+    id: 1,
+    title: 'Hunt 1',
+    selectedPhotos: [],
+    totalPhotos: 5,
+    location: 'San Francisco',
+    model: '911',
+    type: 'Carrera S',
+  },
+  {
+    id: 2,
+    title: 'Hunt 2',
+    selectedPhotos: [],
+    totalPhotos: 5,
+    location: 'San Francisco',
+    model: '911',
+    type: 'Carrera S',
+  },
+  {
+    id: 3,
+    title: 'Hunt 3',
+    selectedPhotos: [],
+    totalPhotos: 5,
+    location: 'San Francisco',
+    model: '911',
+    type: 'Carrera S',
+  },
+  {
+    id: 4,
+    title: 'Hunt 4',
+    selectedPhotos: [],
+    totalPhotos: 5,
+    location: 'San Francisco',
+    model: '911',
+    type: 'Carrera S',
+  },
+  {
+    id: 5,
+    title: 'Hunt 5',
+    selectedPhotos: [],
+    totalPhotos: 5,
+    location: 'San Francisco',
+    model: '911',
+    type: 'Carrera S',
+  },
+]
 
 const Home = ({ navigation, route }) => {
   const { user } = route.params
@@ -19,7 +67,20 @@ const Home = ({ navigation, route }) => {
 
   return (
     <SafeAreaView style={styles.container}>
-      <Text>Home</Text>
+      <Text style={styles.titleContainer}>
+        <Text style={styles.homeTitle}>Porsche</Text>
+        <Text style={styles.homeTitleTwo}>hunts</Text>
+      </Text>
+      <View style={styles.huntListContainer}>
+        <FlatList
+          style={styles.huntList}
+          data={DATA}
+          renderItem={({ item }) => (
+            <Text style={styles.huntItem}>{item.title}</Text>
+          )}
+          keyExtractor={item => item.id}
+        />
+      </View>
     </SafeAreaView>
   )
 }
--- styles/home.js
+++ styles/home.js
@@ -1,11 +1,47 @@
 import { StyleSheet } from 'react-native'
-import { COLORS } from '../constants'
+import { COLORS, FONT } from '../constants'
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: COLORS.primary,
     alignItems: 'center',
+  },
+  titleContainer: {
+    marginTop: 35,
+    marginBottom: 15,
+  },
+  homeTitle: {
+    color: COLORS.gold,
+    fontFamily: FONT.skia,
+    fontSize: 44,
+    textTransform: 'uppercase',
+    textAlign: 'center',
+  },
+  homeTitleTwo: {
+    marginLeft: -5,
+    color: COLORS.lightWhite,
+    fontFamily: FONT.skia,
+    fontSize: 40,
+    fontStyle: 'italic',
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+  huntListContainer: {
+    width: '100%',
+    paddingBottom: 100,
+  },
+  huntList: {
+    borderWidth: 1,
+    borderColor: COLORS.white,
+  },
+  huntItem: {
+    flex: 1,
+    borderWidth: 1,
+    borderColor: COLORS.white,
+    marginVertical: 60,
+    color: COLORS.white,
+    textAlign: 'center',
   },
 })
 
